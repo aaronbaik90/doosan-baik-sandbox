cf4491001b1e65141e14bbc9a4fe8144c9078def
Doosan Baik <dbaik@fb.com>
2017-04-28 13:30:09 -0400

    sample html read

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
-app.engine('html', require('ejs').renderFile);
-
 const ACCESS_TOKEN='EAAZAqLBXCXpgBABxbnT96lc7iZBV4PLg2KJF8eyHZAsUeXJ6ZAUG9YgushGHsoze6XtWhJJGApw4T5RTew1bWZCLBIfgw6wsKh0OPIPZCJMes0R7n6I6ZBe9gnkncmV5lpmxx8pxlGd3BrpIeaM543ITyDymJJ2lHJSuH0Ta9oAPGJJ9aZCH64dBtHN9GOirX2kZD';
 const PAGE_ACCESS_TOKEN='EAAZAqLBXCXpgBAOI43j6Ft5SRvLO79sHWQATgzg5e5Izo71cp3nL5Xu3uWbTRq8Y0nX9ASVZCxAJrX0usHu1gk6ZBCJ0pHgqf4UbQUiiVspetJXSjhvAqqlqsd1MC9AuX3fZC9lSULym04ZCtXe7kj8NTOk9UEziD73J3M6CcWXA5xo8xLGRwFWURF9xICaoZD';
 
@@ -31,12 +29,11 @@ app.post('/webhook', function (req, res) {
     res.sendStatus(200);
 });
 
-app.get('/pixel', function (req, res) {
-  res.render('./pixel.html');
-});
 
 app.get('/sample', function (req, res) {
-  res.render('./sample.html');
+  fs.readFile(__dirname + '/sample.html', 'utf8', function(err, text) {
+    res.send(text);
+  });
 });
 
 app.listen(app.get('port'), function() {
