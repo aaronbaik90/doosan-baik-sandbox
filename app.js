65cf62ff83a46274f13f345c1cfc0fdc3917ad8c
Doosan Baik <dbaik@fb.com>
2017-06-07 21:47:19 -0400

    pixel code implemented

--- app.js
+++ app.js
@@ -31,6 +31,14 @@ app.post('/webhook', function (req, res) {
 });
 
 app.get('/referral', function (req, res) {
+  !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+  n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+  document,'script','https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1837695789816293'); // Insert your pixel ID here.
+  fbq('track', 'PageView');
+  fbq('track', 'Purchase');
   let code = req.param('code');
   res.send(code);
 });
