e3b5c700fc0b013f6449a49e33488da840360784
aaronbaik90 <doosan.baik.90@gmail.com>
2017-10-16 17:54:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-const ACCESS_TOKEN='EAAZAqLBXCXpgBABxbnT96lc7iZBV4PLg2KJF8eyHZAsUeXJ6ZAUG9YgushGHsoze6XtWhJJGApw4T5RTew1bWZCLBIfgw6wsKh0OPIPZCJMes0R7n6I6ZBe9gnkncmV5lpmxx8pxlGd3BrpIeaM543ITyDymJJ2lHJSuH0Ta9oAPGJJ9aZCH64dBtHN9GOirX2kZD';
-const PAGE_ACCESS_TOKEN='EAAZAqLBXCXpgBAOI43j6Ft5SRvLO79sHWQATgzg5e5Izo71cp3nL5Xu3uWbTRq8Y0nX9ASVZCxAJrX0usHu1gk6ZBCJ0pHgqf4UbQUiiVspetJXSjhvAqqlqsd1MC9AuX3fZC9lSULym04ZCtXe7kj8NTOk9UEziD73J3M6CcWXA5xo8xLGRwFWURF9xICaoZD';
-
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || 8080);
